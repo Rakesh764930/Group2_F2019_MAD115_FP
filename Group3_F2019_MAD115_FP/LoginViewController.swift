36a7294657d645221214cbde673849236b738995
Rakesh Kumar <Rakeshkumar92215@gmail.com>
2019-11-12 21:56:57 -0500

    login button outlet created

--- Group3_F2019_MAD115_FP/LoginViewController.swift
+++ Group3_F2019_MAD115_FP/LoginViewController.swift
@@ -13,8 +13,11 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var txtUserName: UITextField!
     @IBOutlet weak var txtPassword: UITextField!
     @IBOutlet weak var swRemember: UISwitch!
+    @IBOutlet weak var loginButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        txtUserName.text = UserDefaults.standard.string(forKey: "usnm")
+        txtPassword.text = UserDefaults.standard.string(forKey: "pswd")
         swRemember.isOn=false
         // Do any additional setup after loading the view.
     }
@@ -24,12 +27,12 @@ class LoginViewController: UIViewController {
                     {
                         if self.swRemember.isOn{
                             
-                            UserDefaults.standard.set(txtUserName.text, forKey: "email")
-                            UserDefaults.standard.set(txtPassword.text, forKey: "pass")
+                            UserDefaults.standard.set(txtUserName.text, forKey: "usnm")
+                            UserDefaults.standard.set(txtPassword.text, forKey: "pswd")
                         }else{
                              
-                            UserDefaults.standard.removeObject(forKey: "userName")
-                            UserDefaults.standard.removeObject(forKey: "userPassword")
+                            UserDefaults.standard.removeObject(forKey: "usnm")
+                            UserDefaults.standard.removeObject(forKey: "pswd")
                             }
 
                                  let stbHome=UIStoryboard(name: "Main", bundle: nil)
@@ -37,13 +40,36 @@ class LoginViewController: UIViewController {
                                         navigationController?.pushViewController(HomeVC, animated: true)
                          }
                              else{
-                                                      let alert = UIAlertController(title: "Invalid Username Or Password", message: "Please Enter Valid username or password", preferredStyle: .alert)
-
-                                                      alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: nil))
-                                                      alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
-                                                      
-                                                      self.present(alert, animated: true)
+            
+                                    let alert = UIAlertController(title: "Invalid Username", message: "Please enter a valid Username", preferredStyle: .alert)
+                                        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                                       alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+                                       alert.addAction(UIAlertAction(title: "Ignore", style: .destructive, handler: nil))
+                                       
+                                       self.present(alert, animated: true)
                                                   }
+                                          }
+                                          
+                                          func readeLoginPlist() -> Bool{
+                                          if let bundlePath = Bundle.main.path(forResource: "eLogin", ofType: "plist") {
+                                              let dictionary = NSMutableDictionary(contentsOfFile: bundlePath)
+
+                                               let usersList = dictionary!["users"] as! NSArray
+                                           
+                                               for u in usersList
+                                               {
+                                                    let user = u as! NSDictionary
+                                                   let usnm = user["usnm"]! as! String
+                                                   let pswd = user["pswd"]! as! String
+                                                   if usnm==txtUserName.text! && pswd==txtPassword.text!
+                                                   {
+                                                       return true
+                                                   }
+                                               }
+                                           
+                                          
+                                              }
+                                               return false
                                           }
                                           
                                                
