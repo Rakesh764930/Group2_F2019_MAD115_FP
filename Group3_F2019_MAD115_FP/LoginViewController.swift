44a8d13727445cc56b83df4480fa56b69f06939d
Rakesh Kumar <Rakeshkumar92215@gmail.com>
2019-11-12 20:57:30 -0500

    view controller added for login coding

---
+++ Group3_F2019_MAD115_FP/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  Group3_F2019_MAD115_FP
+//
+//  Created by MacStudent on 2019-11-12.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
