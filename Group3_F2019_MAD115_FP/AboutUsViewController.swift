46296429ab9e6d31bc58e1199315f58fd024e85c
Rajdeep Kaur <chahal.rajdeep03@gmail.com>
2019-11-15 18:40:13 -0500

    about us screen

--- Group3_F2019_MAD115_FP/AboutUsViewController.swift
+++ Group3_F2019_MAD115_FP/AboutUsViewController.swift
@@ -11,21 +11,11 @@ import WebKit
 
 class AboutUsViewController: UIViewController {
 
+    @IBOutlet weak var aboutWK: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+            //self.loadFromURL()        // Do any additional setup after loading the view.
+        }
+        
 }
