cc492a07ffdd59f221e8436f37d3adf38cdf0de8
Rajdeep Kaur <chahal.rajdeep03@gmail.com>
2019-11-15 15:11:09 -0500

    make call function in contact us view controller

--- Group3_F2019_MAD115_FP/ContactUsViewController.swift
+++ Group3_F2019_MAD115_FP/ContactUsViewController.swift
@@ -28,8 +28,30 @@ class ContactUsViewController: UIViewController {
                 
                 alert.addAction(okAction)
                 
+                let callAction = UIAlertAction(title: "Call Us",
+                                                   style: .default, handler: {
+                                                    _ in
+                                                    self.makeCall()
+                    })
+                    alert.addAction(callAction)
+                    
+                    
+                }
                 
-        }    /*
+                private func makeCall()
+                {
+                    if let url = URL(string: "tel://+14379233497)"), UIApplication.shared.canOpenURL(url){
+                        if #available(iOS 10, *)
+                        {
+                            UIApplication.shared.open(url)
+                        }
+                        else
+                        {
+                            UIApplication.shared.openURL(url)
+                        }
+                    }
+                }
+                    }    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
