e95cc17f79e3588bf3c40c8e6d4184b8812e9aa4
Ajeet Singh <ajeets110@gmail.com>
2019-11-16 17:05:36 -0500

    made another init for fixed based part time

--- Group3_F2019_MAD115_FP/Swift_Payroll_Project/Employee/FixedBasedPartTime.swift
+++ Group3_F2019_MAD115_FP/Swift_Payroll_Project/Employee/FixedBasedPartTime.swift
@@ -21,6 +21,11 @@ class FixedBasedPartTime : PartTime
     
     var fixedAmount:Float
     
+    init(id:Int,fixedAmount:Float, rate:Float, hoursWorked:Float, name:String, age:Int, type:String) {
+        self.fixedAmount=fixedAmount
+        super.init(id: id, rate:rate,hoursWorked:hoursWorked,name:name,age:age, type: type)
+    }
+    
     init(fixedAmount:Float, rate:Float, hoursWorked:Float, name:String, age:Int, type:String) {
         let id = sinObj.empDict.count+1
         self.fixedAmount=fixedAmount
