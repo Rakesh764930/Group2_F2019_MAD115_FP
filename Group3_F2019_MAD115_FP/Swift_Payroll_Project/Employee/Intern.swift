af48f68daae1fa3562a5a46002a5f33ab1c41eae
Ajeet Singh <ajeets110@gmail.com>
2019-11-16 17:03:25 -0500

    another init for Intern for default values

--- Group3_F2019_MAD115_FP/Swift_Payroll_Project/Employee/Intern.swift
+++ Group3_F2019_MAD115_FP/Swift_Payroll_Project/Employee/Intern.swift
@@ -10,6 +10,13 @@ import Foundation
 class Intern:Employee {
     private var schoolName:String
     var sinObj=Singleton.getInstance()
+    
+    init(id:Int,name:String,age:Int,schoolName:String,type:String) {
+        self.schoolName=schoolName
+        super.init(id: id, name:name,age:age,type:type)
+    
+    }
+    
     init(name:String,age:Int,schoolName:String,type:String) {
         self.schoolName=schoolName
         let id=sinObj.empDict.count+1
