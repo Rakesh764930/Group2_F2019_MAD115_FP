e1351b3900b1b514b3d90d34627b9bd0820de0fe
Ajeet Singh <ajeets110@gmail.com>
2019-11-15 21:08:06 -0500

    created an extension for string to int

---
+++ Group3_F2019_MAD115_FP/StringToInt.swift
@@ -0,0 +1,15 @@
+//
+//  StringToInt.swift
+//  Group3_F2019_MAD115_FP
+//
+//  Created by MacStudent on 2019-11-15.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+extension String{
+    func stringToInt() -> Int
+    {
+        return Int(self)!
+    }
+}
