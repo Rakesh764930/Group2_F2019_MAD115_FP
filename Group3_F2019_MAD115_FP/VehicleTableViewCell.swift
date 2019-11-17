298b249a4162d661b6ec81e311a5306ab5e23d68
Ajeet Singh <ajeets110@gmail.com>
2019-11-16 19:38:02 -0500

    created a table view cell for vehicle

---
+++ Group3_F2019_MAD115_FP/VehicleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  VehicleTableViewCell.swift
+//  Group3_F2019_MAD115_FP
+//
+//  Created by MacStudent on 2019-11-16.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class VehicleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
