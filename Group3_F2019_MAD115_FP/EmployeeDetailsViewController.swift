f0d8e6122b212aca1ad918ad1a3e23d936b2386a
Ajeet Singh <ajeets110@gmail.com>
2019-11-16 19:35:59 -0500

    added labels

--- Group3_F2019_MAD115_FP/EmployeeDetailsViewController.swift
+++ Group3_F2019_MAD115_FP/EmployeeDetailsViewController.swift
@@ -8,23 +8,77 @@
 
 import UIKit
 
-class EmployeeDetailsViewController: UIViewController {
+class EmployeeDetailsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (e?.vdict.count)!
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 
-
+    @IBOutlet weak var empID: UILabel!
+    @IBOutlet weak var empName: UILabel!
+    
+    @IBOutlet weak var dateOfBirth: UILabel!
+    @IBOutlet weak var empType: UILabel!
+    @IBOutlet weak var totalEarn: UILabel!
+    
+    
+    @IBOutlet weak var lbl1: UILabel!
+    @IBOutlet weak var lbl2: UILabel!
+    
+    @IBOutlet weak var lbl3: UILabel!
+    @IBOutlet weak var lbl1_1: UILabel!
+    @IBOutlet weak var lbl2_2: UILabel!
+    @IBOutlet weak var lbl3_3: UILabel!
+    var e : Employee?=nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    var e : Employee?=nil
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+        empID.text = e?.empId.intToStr()
+        empName.text = e?.name
+        dateOfBirth.text = e?.dateOfBirth.intToStr()
+        empType.text = e?.type
+        totalEarn.text = e?.calcEarnings().floatToStr()
+        
+        if e?.type == "FullTime"{
+            lbl1.text = "Salary"
+            let f = e as! FullTime
+            lbl1_1.text = f.salary.floatToStr()
+            lbl2.text = "Bonus"
+            lbl2_2.text = f.bonus.floatToStr()
+        }
+        if e?.type == "Intern"{
+        lbl1.text = "School Name"
+        let i = e as! Intern
+            lbl1_1.text = i.schoolName
+        
+        }
+        
+        if e?.type == "FixedBasedPartTime"{
+        lbl1.text = "Fixed Amount"
+            lbl2.text = "Rate"
+            lbl3.text = "Hours Worked"
+        let fix = e as! FixedBasedPartTime
+            lbl1_1.text = fix.fixedAmount.floatToStr()
+            lbl2_2.text = fix.rate.floatToStr()
+            lbl3_3.text = fix.hoursWorked.floatToStr()
+        }
+        
+        if e?.type == "CommissionBasedPartTime"{
+        lbl1.text = "Commission Percentage"
+            lbl2.text = "Hours Wroked"
+            lbl3.text = "Rate"
+        let comm = e as! CommissionBasedPartTime
+            lbl1_1.text = comm.commissionPerc.floatToStr()
+            lbl2_2.text = comm.hoursWorked.floatToStr()
+            lbl3_3.text = comm.rate.floatToStr()
+        }
+}
+    
 }
