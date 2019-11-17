13fecc7af82224d1b0ecf6091ab8ab8fb70fb19f
Ajeet Singh <ajeets110@gmail.com>
2019-11-16 19:36:21 -0500

    created extension

---
+++ Group3_F2019_MAD115_FP/IntToStringExtension.swift
@@ -0,0 +1,14 @@
+//
+//  IntToStringExtension.swift
+//  Group3_F2019_MAD115_FP
+//
+//  Created by MacStudent on 2019-11-16.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+extension Int{
+    func intToStr() -> String {
+        return String(self)
+    }
+}
