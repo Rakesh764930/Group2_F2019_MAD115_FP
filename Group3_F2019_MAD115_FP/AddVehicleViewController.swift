9f8a43439f0a597aa26d1f8ecced096a39f139b2
Ajeet Singh <ajeets110@gmail.com>
2019-11-18 14:37:51 -0500

    created add vehicle view controller

---
+++ Group3_F2019_MAD115_FP/AddVehicleViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AddVehicleViewController.swift
+//  Group3_F2019_MAD115_FP
+//
+//  Created by MacStudent on 2019-11-18.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class AddVehicleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+}
