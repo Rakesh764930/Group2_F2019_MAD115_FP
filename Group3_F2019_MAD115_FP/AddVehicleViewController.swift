713f336d524717219c7e4dbfb4e2f6cce8409c4e
Ajeet Singh <ajeets110@gmail.com>
2019-11-18 15:23:08 -0500

    added aadd vehicle

--- Group3_F2019_MAD115_FP/AddVehicleViewController.swift
+++ Group3_F2019_MAD115_FP/AddVehicleViewController.swift
@@ -18,6 +18,10 @@ class AddVehicleViewController: UIViewController {
     @IBOutlet weak var brandTxt: UITextField!
     @IBOutlet weak var brandLabel: UILabel!
     @IBOutlet weak var numberOfSeatsLabel: UILabel!
+    
+    
+    var employee:Employee?=nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -32,11 +36,55 @@ class AddVehicleViewController: UIViewController {
             vehicleBrand.isHidden = false
             vehicleNumberOfSeats.isHidden = false
             brandTxt.isHidden = false
-            vehicleNumberOfSeats.isHidden = false
+            numberOfSeatsLabel.isHidden = false
+            
         case 1:
             brandLabel.text = "Model"
             numberOfSeatsLabel.isHidden = true
             vehicleNumberOfSeats.isHidden = true
+        default:
+            break
+        }
+        
+    }
+
+    @IBAction func addVehcle(_ sender: Any) {
+        let make = vehicleMake.text!
+        let plate = vehiclePlate.text!
+        switch vehicleType.selectedSegmentIndex {
+        case 0:
+            let type = "Car"
+            let brand = brandTxt.text!
+            let numberOfSeats = vehicleNumberOfSeats.text!
+            if(make == nil || plate == nil || type == nil || brand == nil ){
+                
+                let alert = UIAlertController(title: "Empty Field", message: "Please Fill all the details", preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                
+                self.present(alert, animated: true)
+                
+            }else{
+                let car = Car(brand: brand, numberOfSeats: numberOfSeats.stringToInt(), make: make.stringToInt(), plate: plate, type: type)
+                employee?.addVehicle(v: car)
+            }
+        case 1:
+            let type = "Motorcycle"
+            let model = brandTxt.text!
+            
+            if(make == nil || plate == nil || type == nil || model == nil){
+                
+                let alert = UIAlertController(title: "Empty Field", message: "Please Fill all the details", preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                
+                self.present(alert, animated: true)
+                
+            }else{
+                let motorcycle = Motorcycle(model: model, type: type, plate: plate, make: make.stringToInt())
+                employee?.addVehicle(v: motorcycle)
+            }
+            
         default:
             break
         }
